aca22892b3f6478e58d477724bc4fe649ab58e58
Nathan Anderson <nathana@microsoft.com>
2016-04-11 21:24:39 +0000

    add to travis file, add documentation and clean up install script

--- frameworks/CSharp/aspnetcore/README.md
+++ frameworks/CSharp/aspnetcore/README.md
@@ -1,2 +1,25 @@
 # ASP.NET Core placeholder
 
+## Tests
+
+* JSON serialization
+* Plaintext
+
+## Versions
+
+**Language**
+
+* C# 5.0
+
+**Platforms**
+
+* .NET Core
+
+**Web Servers**
+
+* Kestrel
+
+**Web Stack**
+
+* ASP.NET Core
+
--- toolset/setup/linux/frameworks/dotnetcore.sh
+++ toolset/setup/linux/frameworks/dotnetcore.sh
@@ -5,18 +5,24 @@ RETCODE=$(fw_exists ${IROOT}/dotnetcore.installed)
 source $IROOT/dotnetcore.installed
 return 0; }
 
-wget https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-host-ubuntu-x64.latest.deb
-wget https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-sharedframework-ubuntu-x64.latest.deb
-wget https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-sdk-ubuntu-x64.latest.deb
+CORE_HOME=$IROOT/dotnetcore
+if [ -d $CORE_HOME ]; then
+    rm -rf $CORE_HOME
+fi
+
+mkdir $CORE_HOME
+cd $CORE_HOME
+
+fw_get -o dotnet-host-ubuntu-x64.latest.deb https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-host-ubuntu-x64.latest.deb
+fw_get -o dotnet-sharedframework-ubuntu-x64.latest.deb https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-sharedframework-ubuntu-x64.latest.deb
+fw_get -o dotnet-sdk-ubuntu-x64.latest.deb https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-sdk-ubuntu-x64.latest.deb
 
 sudo apt-get install -y liblldb-3.6 liblttng-ust0 libunwind8 clang-3.5
 sudo apt-get install -f -y
 sudo dpkg -i dotnet-host-ubuntu-x64.latest.deb
 sudo dpkg -i dotnet-sharedframework-ubuntu-x64.latest.deb
 sudo dpkg -i dotnet-sdk-ubuntu-x64.latest.deb
-
-echo DotnetCore installed
-
+dotnet --version
 
 touch $IROOT/dotnetcore.installed
 
